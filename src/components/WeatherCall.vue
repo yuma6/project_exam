a1af973691f77338dfa0ca3da20aea6a98ec3080
yumayumayuma <yumapuripuri@gmail.com>
2021-07-06 14:34:13 +0900

    apiコメントアウト

--- src/components/WeatherCall.vue
+++ src/components/WeatherCall.vue
@@ -1,17 +1,17 @@
 <script>
-import axios from "axios"
-export default {
-    data () {
-          return {
-            info: null
-        }
-    },
-    mounted () {
-      let self = this
-      axios.get('http://wttr.in/Fukuoka?format=j1')
-      .then(function(response){(self.info = response)})
-      // .then(function(res){console.log(res)})
-      // .then(function(){console.log(self.info)})
-    }
-}
+// import axios from "axios"
+// export default {
+//     data () {
+//           return {
+//             info: null
+//         }
+//     },
+//     mounted () {
+//       let self = this
+//       axios.get('http://wttr.in/Fukuoka?format=j1')
+//       .then(function(response){(self.info = response)})
+//       // .then(function(res){console.log(res)})
+//       // .then(function(){console.log(self.info)})
+//     }
+// }
 </script>
